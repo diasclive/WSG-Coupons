ce037e1cc7f92b9fb216c4fd8fcdd572bd28cdf8
Glen D'souza <gdsouza@linuxmail.org>
2016-07-12 12:41:57 +0530

    Clean up the admin view a bit

--- coupons/admin.py
+++ coupons/admin.py
@@ -6,13 +6,14 @@ class CouponAdmin(admin.ModelAdmin):
     fieldsets = [
         ('Basic Information', {'fields': ['title','code','owner']}),
         ('Date Information', {'fields': ['create_date','publish_date','validity']}),
-        ('Terms & Conditions', {'fields': ['terms']}),
+        ('Terms & Conditions', {'fields': ['terms'], 'classes': ['collapse']}),
     ]
     list_display = ('title', 'code', 'published', 'publish_date')
     list_filter = ['publish_date']
     search_fields = ['title']
     readonly_fields = ('create_date','owner',)
     save_as = True
+    list_per_page = 10
 
     def queryset(self, request):
         return Coupon.objects.filter(
@@ -21,4 +22,5 @@ class CouponAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         obj.save()
+
 admin.site.register(Coupon, CouponAdmin)
