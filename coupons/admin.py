2a71b026bf4a41b3be3e22f81d306cfec8fcc3e3
Glen D'souza <gdsouza@linuxmail.org>
2016-07-12 11:33:34 +0530

    Update model & Set timezone

--- coupons/admin.py
+++ coupons/admin.py
@@ -3,16 +3,22 @@ from django.contrib import admin
 from .models import Coupon
 
 class CouponAdmin(admin.ModelAdmin):
-    """fieldsets = [
-        (None,               {'fields': ['title']}),
-        ('Date information', {'fields': ['publish_date']}),
-    ]"""
+    fieldsets = [
+        ('Basic Information', {'fields': ['title','code','owner']}),
+        ('Date Information', {'fields': ['create_date','publish_date','validity']}),
+        ('Terms & Conditions', {'fields': ['terms']}),
+    ]
     list_display = ('title', 'code', 'published', 'publish_date')
     list_filter = ['publish_date']
     search_fields = ['title']
+    readonly_fields = ('create_date','owner',)
+    save_as = True
 
     def queryset(self, request):
         return Coupon.objects.filter(
             owner=request.user)
 
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        obj.save()
 admin.site.register(Coupon, CouponAdmin)
--- coupons/models.py
+++ coupons/models.py
@@ -6,28 +6,31 @@ from django.utils import timezone
 # Coupon Class Specifier
 class Coupon(models.Model):
     # Creator of coupon
-    owner = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+        editable=False)
     title = models.CharField(max_length=100)
     code = models.CharField(max_length=10, primary_key=True)
     terms = models.TextField()
 #   claimants = models.ManyToManyField(Person, through='Claim')
 
     create_date = models.DateTimeField(
-        default=timezone.now)
+        default=timezone.now, editable=False)
     # When the coupon becomes ready for use
     publish_date = models.DateTimeField(
-        default=timezone.now() + datetime.timedelta(days=1))
-    # When this coupon expires
-    end_date = models.DateTimeField(
         default=timezone.now)
-
-    def publish(self):
-        self.publish_date = timezone.now()
-        self.save()
+    # When this coupon expires
+    validity = models.DateTimeField(
+        default=timezone.now)
+    '''
+    # Use this method when we get to postgres
+    validity = models.DurationField(
+        default=datetime.timedelta(days=7), help_text="1 12:00 = 1 day + 12 hours")'''
 
     # Method to determine if coupon is currently published or not
     def published(self):
-        return self.publish_date <= timezone.now() <= self.end_date
+        return self.publish_date <= timezone.now() <= self.validity
     published.admin_order_field = 'publish_date'
     published.boolean = True
     published.short_description = 'Currently Running'
