b96415122a33940285536edb51b8031108f23433
Glen D'souza <gdsouza@linuxmail.org>
2016-07-22 18:33:05 +0530

    Add templates for password reset mechanism

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,11 +7,11 @@ app_name = 'accounts'
 urlpatterns = [
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
-    url(r'^password_change/$', auth_views.password_change, name='password_change'),
+    url(r'^password_change/$', auth_views.password_change, {'post_reset_redirect':'accounts:password_change_done', }, name='password_change'),
     url(r'^password_change/done/$', auth_views.password_change_done, name='password_change_done'),
-    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/$', auth_views.password_reset, {'post_reset_redirect':'accounts:password_reset_done', }, name='password_reset'),
     url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
     url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-        auth_views.password_reset_confirm, name='password_reset_confirm'),
+        auth_views.password_reset_confirm, {'post_reset_redirect':'accounts:password_reset_complete', }, name='password_reset_confirm'),
     url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 ]
