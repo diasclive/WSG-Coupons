326b403d7152c5b1533777d78c1e86e110629bcb
Glen D'souza <gdsouza@linuxmail.org>
2016-09-13 14:54:44 +0530

    WSG: Update Email Settings

--- WSG/settings.py
+++ WSG/settings.py
@@ -146,14 +146,13 @@ STATICFILES_DIRS = [
 
 # Email Settings
 
-DEFAULT_FROM_EMAIL = 'webmaster@example.com'
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST = 'localhost'
-
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
 
 EMAIL_HOST_USER = ''
-
-EMAIL_PORT = 25
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
